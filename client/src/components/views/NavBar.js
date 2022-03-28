291be66329e1c19e8f1620c922e3a9748b9034e5
Triplejblog <triplej03@naver.com>
2022-03-27 23:27:53 -0700

    Navigation

---
+++ client/src/components/views/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <nav>
+        <Link to="/">Home</Link> |<Link to="about">About</Link> |
+        <Link to="Dashboard">Dashboard</Link>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/views/NavBar";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<App />}></Route> */}
+      <Route index element={<NavBar />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
