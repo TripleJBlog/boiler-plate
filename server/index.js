4a4331bf2513019b546a1c9d096a44f8e1a2ad29
Triplejblog <triplej03@naver.com>
2022-03-31 22:46:22 -0700

    bcrypt and jwt for login

--- server/index.js
+++ server/index.js
@@ -3,13 +3,15 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const mongoose = require("mongoose");
-const port = process.env.SERVER_PORT || 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const port = process.env.SERVER_PORT || 5000;
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require("./config/key");
 const { User } = require("./models/User");
@@ -29,6 +31,35 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+app.post("/login", (req, res) => {
+  // 1. Look up requested email from DB
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "There is no user with that email.",
+      });
+    }
+    // 2. If email exists in DB, check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        res.json({
+          loginSuccess: false,
+          message: "Password is not matched",
+        });
+      }
+    });
+    // 3. Generate Token to Cookie
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+        userId: user._id,
+      });
     });
   });
 });
